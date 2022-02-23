53e82c1efe11ef715f2a6329640f986818db691c
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-02-23 03:39:58 +0100

    Added url parameters to review viewset

--- django/reviewApp/views.py
+++ django/reviewApp/views.py
@@ -45,6 +45,19 @@ class AlbumOfTheYearViewSet(ModelViewSet):
 
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     pagination_class = pagination.FivePagesPagination
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against `album_id` and `reviewer_id` query parameter in the URL.
+        """
+        queryset = Review.objects.all()
+        album_id = self.request.query_params.get('album_id')
+        reviewer_id = self.request.query_params.get('reviewer_id')
+        if album_id is not None:
+            queryset = queryset.filter(album_id=album_id)
+        if reviewer_id is not None:
+            queryset = queryset.filter(reviewer_id=reviewer_id)
+        return queryset
