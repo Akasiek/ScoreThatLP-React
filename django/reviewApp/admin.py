c56e883a99062ba54f7b2378943d4bfaa996c057
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-02-24 20:52:36 +0100

    Small changes in admin panel

--- django/reviewApp/admin.py
+++ django/reviewApp/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Reviewer)
+class ReviewerAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["user"]
+    list_display = ["username", "email"]
+
+
 @admin.register(models.Album)
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ["title"]
@@ -10,6 +16,8 @@ class AlbumAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ["title"]
     }
+    autocomplete_fields = ["artist_id"]
+    search_fields = ['title__istartswith']
 
 
 @admin.register(models.Artist)
@@ -20,21 +28,24 @@ class ArtistAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ["name"]
     }
+    search_fields = ['name__istartswith']
 
 
 @admin.register(models.Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name__istartswith']
 
 
 @admin.register(models.AlbumGenre)
 class AlbumGenreAdmin(admin.ModelAdmin):
     list_display = ["__str__", "album_id", "genre_id"]
+    autocomplete_fields = ["album_id", "genre_id"]
 
 
 @admin.register(models.AlbumLink)
 class AlbumLinkAdmin(admin.ModelAdmin):
     list_display = ["__str__", "album_id"]
+    autocomplete_fields = ["album_id"]
 
 
 @admin.register(models.AlbumOfTheYear)
