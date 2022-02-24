34840415f84892527fffbef1dcd10fa935982e3b
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-02-24 20:53:44 +0100

    Create permissions.py

---
+++ django/reviewApp/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+
+class IsAdminOrPostOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif request.method == "POST":
+            return bool(request.user and request.user.is_authenticated)
+        return bool(request.user and request.user.is_staff)
