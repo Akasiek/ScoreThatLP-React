c00d1070466b9dab2da23e9c2e844fcda0f2fbc4
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-03-08 23:50:03 +0100

    Added prompt for not released albums to prevent from rating

--- react/src/components/albumPage/albumPageReviewSection.jsx
+++ react/src/components/albumPage/albumPageReviewSection.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import moment from "moment";
+
 import AlbumPageReviewFormContainer from "./reviewForm/reviewFormContainer";
 import AlbumPageReviewsContainer from "./albumPageReviewsContainer";
 
 const StyledReviewSection = styled.section`
     margin: 3rem;
 
-    .loginPrompt {
+    .prompt {
         font-size: clamp(1rem, 1.5vw, 1.5rem);
         a {
             color: var(--accentColor);
@@ -29,12 +31,16 @@ const StyledReviewSection = styled.section`
 const AlbumPageReviewSection = ({ album, user }) => {
     return (
         <StyledReviewSection>
-            {user ? (
-                <AlbumPageReviewFormContainer album={album} user={user} />
+            {moment(album.release_date) < moment() ? (
+                user ? (
+                    <AlbumPageReviewFormContainer album={album} user={user} />
+                ) : (
+                    <div className="prompt">
+                        You need to be logged in as user to review albums. <Link to="/login">Log in here</Link>
+                    </div>
+                )
             ) : (
-                <div className="loginPrompt">
-                    You need to be logged in as user to review albums. <Link to="/login">Log in here</Link>
-                </div>
+                <div className="prompt">The album is waiting for a release. Time to release: {moment(album.release_date).toNow(true)}</div>
             )}
             <AlbumPageReviewsContainer album={album} />
         </StyledReviewSection>
