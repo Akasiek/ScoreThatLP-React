7c9f1efa493338987164410e3915cf161aced492
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-03-13 20:47:00 +0100

    Update logService.js

--- react/src/services/logService.js
+++ react/src/services/logService.js
@@ -1,10 +1,10 @@
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/tracing";
 
 function init() {
     Sentry.init({
         dsn: "https://f274741b8faf4992a13a1757dd539025@o1118067.ingest.sentry.io/6221943",
-        integrations: [new Integrations.BrowserTracing()],
+        integrations: [new BrowserTracing()],
 
         // Set tracesSampleRate to 1.0 to capture 100%
         // of transactions for performance monitoring.
