a671817f85739104c865f3258fa2c472a8ee5be2
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-03-15 22:42:08 +0100

    Refactoring

--- react/src/services/reviewerService.js
+++ react/src/services/reviewerService.js
@@ -2,26 +2,14 @@ import http from "./httpService";
 
 const apiEndpoint = "/reviewers/";
 
-export const getReviewer = (id) => {
-    return http.get(`${apiEndpoint}${id}`);
-};
+export const getReviewer = (id) => http.get(`${apiEndpoint}${id}`);
 
-export const getReviewerWithUser = (user_id) => {
-    return http.get(`${apiEndpoint}?user=${user_id}`);
-};
+export const getReviewerWithUser = (user_id) => http.get(`${apiEndpoint}?user=${user_id}`);
 
-export const getReviewerByUsername = (username) => {
-    return http.get(`${apiEndpoint}?user__username=${username}`);
-};
+export const getReviewerByUsername = (username) => http.get(`${apiEndpoint}?user__username=${username}`);
 
-export const createReviewer = (user, headers = null) => {
-    return http.post(`${apiEndpoint}`, user, { headers: headers });
-};
+export const createReviewer = (user, headers = null) => http.post(apiEndpoint, user, { headers: headers });
 
-export const updateReviewer = (reviewer_id, reviewer) => {
-    return http.patch(`${apiEndpoint}${reviewer_id}/`, reviewer);
-};
+export const updateReviewer = (reviewer_id, reviewer) => http.patch(`${apiEndpoint}${reviewer_id}/`, reviewer);
 
-export const searchReviewers = (query) => {
-    return http.get(`${apiEndpoint}?search=${query}`);
-};
+export const searchReviewers = (query) => http.get(`${apiEndpoint}?search=${query}`);
