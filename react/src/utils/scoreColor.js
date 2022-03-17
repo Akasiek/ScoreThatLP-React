e6a343c2d0b26a70eb64b14b8d868291df02c653
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-03-17 18:43:50 +0100

    Changed score colors values

--- react/src/utils/scoreColor.js
+++ react/src/utils/scoreColor.js
@@ -1,7 +1,7 @@
 export default function getScoreColor(score) {
     score = parseInt(score);
-    if (score <= 100 && score > 60) return "Green";
-    if (score <= 60 && score >= 40) return "Yellow";
+    if (score <= 100 && score > 74) return "Green";
+    if (score <= 74 && score >= 40) return "Yellow";
     if (score < 40 && score >= 0) return "Red";
     return null;
 }
